f776238b3b20e886e307173cd3f5e727e596d225
HQuang123 <100994017+HQuang123@users.noreply.github.com>
2024-01-28 22:00:17 +0700

    commit gcd

---
+++ PRF192/gcd.c
@@ -0,0 +1,30 @@
+/* file pointer_demo5.c */ 
+#include <stdio.h>
+#include <math.h>
+int gcd(int a, int b){
+	while(a!=b){
+		if (a>b){a-=b;
+		}
+		else{b-=a;
+		}
+	}
+}
+
+int lcm(int a, int b){
+	return a*b/gcd(a,b);
+}
+
+
+int main(){
+	int a,b;
+	do {
+		scanf("%d %d", &a, &b);
+	}
+	while(a<0||b<0);
+	printf("The greatest common divisor is %d\n", gcd(a,b));
+	printf("The least common multiple is %d", lcm(a,b));
+	return 0;
+}
+
+
+
